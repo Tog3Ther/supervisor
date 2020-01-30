aaae98ccfb43bf616a9093fc71dbb724b5cbeef2
Tog3Ther <Ustymenko.m@gmail.com>
2020-01-30 21:24:55 +0300

    master

--- MindyData/settings.py
+++ MindyData/settings.py
@@ -117,10 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-STATIC_URL = '/static/'
+STATIC_ROOT = "/home/Tog3Ther/tog3ther.pythonanywhere.com/static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 mimetypes.add_type("text/css", ".css", True)
